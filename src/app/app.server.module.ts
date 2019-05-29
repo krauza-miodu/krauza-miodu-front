b4250a1c44656703d537ff4a83c59c8b3d26b5df
Viktor <Nivelis91@gmail.com>
2019-05-29 21:05:04 +0200

    Updates linter rules

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
-import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
-    ModuleMapLoaderModule,
+    ModuleMapLoaderModule
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppServerModule {}
